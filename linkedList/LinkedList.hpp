469a2b874e0a7ef8214959c99c60cd2165dc8153
Deepak <d045k281@ku.edu>
2020-09-05 11:56:44 -0500

    Changed size function Definition

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -1,6 +1,6 @@
 /**
-*	@author 
-*	@date 
+*	@author:Deepak Kumar
+*	@date :09/04/2020
 *	@file LinkedList.hpp
 *	@brief Implementation file for templated LinkedList class
 */
@@ -29,10 +29,9 @@ bool LinkedList<T>::isEmpty() const
 template <typename T>
 int LinkedList<T>::size() const
 {
-	/** TODO 
-		Fix this method
-	*/
-	return(0);
+	return(m_size); 
+	
+	
 }
 
 template <typename T>
